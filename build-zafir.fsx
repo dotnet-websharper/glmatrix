b2546e0608e08271652e71e610a0489893995125
István Gansperger <gansperger@gmail.com>
2015-12-22 11:59:42 +0100

    Ivy fix

--- build-zafir.fsx
+++ build-zafir.fsx
@@ -19,7 +19,7 @@ let test =
         .References(fun r ->
             [
                 r.Project main
-                r.NuGet("Zafir.Html").Reference()
+                r.NuGet("Zafir.Html").Latest(true).ForceFoundVersion().Reference()
             ])
 
 bt.Solution [
