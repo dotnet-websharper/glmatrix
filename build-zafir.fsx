a2215ad1d9303c0b03057ec4a9e3bcd6e7861cb2
Jand42 <andras.janko@gmail.com>
2017-09-05 21:46:02 +0200

    Rename Zafir packages to WebSharper

--- build-zafir.fsx
+++ build-zafir.fsx
@@ -2,24 +2,24 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("Zafir.GlMatrix")
-        .VersionFrom("Zafir")
+    BuildTool().PackageId("WebSharper.GlMatrix")
+        .VersionFrom("WebSharper")
         .WithFSharpVersion(FSharpVersion.FSharp30)
         .WithFramework(fun fw -> fw.Net40)
         .References(fun r -> [r.Assembly "System.Web"])
 
 let main =
-    bt.Zafir.Extension("WebSharper.GlMatrix")
+    bt.WebSharper4.Extension("WebSharper.GlMatrix")
         .Embed(["gl-matrix-min.js"])
         .SourcesFromProject()
 
 let test =
-    bt.Zafir.HtmlWebsite("WebSharper.GlMatrix.Tests")
+    bt.WebSharper4.HtmlWebsite("WebSharper.GlMatrix.Tests")
         .SourcesFromProject()
         .References(fun r ->
             [
                 r.Project main
-                r.NuGet("Zafir.Html").Latest(true).ForceFoundVersion().Reference()
+                r.NuGet("WebSharper.Html").Latest(true).ForceFoundVersion().Reference()
             ])
 
 bt.Solution [
@@ -29,10 +29,10 @@ bt.Solution [
     bt.NuGet.CreatePackage()
         .Configure(fun c ->
             { c with
-                Title = Some "Zafir.GlMatrix-2.2.0"
+                Title = Some "WebSharper.GlMatrix-2.2.0"
                 LicenseUrl = Some "http://websharper.com/licensing"
                 ProjectUrl = Some "https://github.com/intellifactory/websharper.glmatrix"
-                Description = "Zafir Extensions for glMatrix 2.2.0"
+                Description = "WebSharper Extensions for glMatrix 2.2.0"
                 RequiresLicenseAcceptance = true })
         .Add(main)
 
