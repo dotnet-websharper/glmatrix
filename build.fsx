8d3f0971ccedb13fdd700a22f8afe23dd22132ce
Loic Denuziere <loic@denuziere.net>
2018-01-22 20:10:50 +0100

    Convert to .NET Standard

--- build.fsx
+++ build.fsx
@@ -1,40 +1,17 @@
-#load "tools/includes.fsx"
-open IntelliFactory.Build
+#load "paket-files/build/intellifactory/websharper/tools/WebSharper.Fake.fsx"
+open Fake
+open WebSharper.Fake
 
-let bt =
-    BuildTool().PackageId("WebSharper.GlMatrix")
-        .VersionFrom("WebSharper", versionSpec = "(,4.0)")
-        .WithFSharpVersion(FSharpVersion.FSharp30)
-        .WithFramework(fun fw -> fw.Net40)
-        .References(fun r -> [r.Assembly "System.Web"])
+let targets =
+    GetSemVerOf "WebSharper"
+    |> ComputeVersion
+    |> WSTargets.Default
+    |> MakeTargets
 
-let main =
-    bt.WebSharper.Extension("WebSharper.GlMatrix")
-        .Embed(["gl-matrix-min.js"])
-        .SourcesFromProject()
+Target "Build" DoNothing
+targets.BuildDebug ==> "Build"
 
-let test =
-    bt.WebSharper.HtmlWebsite("WebSharper.GlMatrix.Tests")
-        .SourcesFromProject()
-        .References(fun r ->
-            [
-                r.Project main
-                r.NuGet("WebSharper.Html").Version("(,4.0)").Reference()
-            ])
+Target "CI-Release" DoNothing
+targets.CommitPublish ==> "CI-Release"
 
-bt.Solution [
-    main
-    test
-
-    bt.NuGet.CreatePackage()
-        .Configure(fun c ->
-            { c with
-                Title = Some "WebSharper.GlMatrix-2.2.0"
-                LicenseUrl = Some "http://websharper.com/licensing"
-                ProjectUrl = Some "https://github.com/intellifactory/websharper.glmatrix"
-                Description = "WebSharper Extensions for glMatrix 2.2.0"
-                RequiresLicenseAcceptance = true })
-        .Add(main)
-
-]
-|> bt.Dispatch
+RunTargetOrDefault "Build"
